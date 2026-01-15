c9b42ab4fc54d9c852bad70d4c01431234edf0e6
7174Andy <hep003@ucsd.edu>
2026-01-14 21:37:58 -0800

    remove sudo

--- lablink-infrastructure/config/custom-startup.sh
+++ lablink-infrastructure/config/custom-startup.sh
@@ -1,4 +1,4 @@
 echo "This is a custom startup script running inside the client container."
 
 # Install SLEAP
-sudo uv tool install --python 3.13 "sleap[nn]" --index https://download.pytorch.org/whl/cu128 --index https://pypi.org/simple
+uv tool install --python 3.13 "sleap[nn]" --index https://download.pytorch.org/whl/cu128 --index https://pypi.org/simple
