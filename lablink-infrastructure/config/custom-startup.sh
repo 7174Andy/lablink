91a9d0263aec836bc812c4171cdb970681aa703e
7174Andy <hep003@ucsd.edu>
2026-01-15 09:53:59 -0800

    add path setting

--- lablink-infrastructure/config/custom-startup.sh
+++ lablink-infrastructure/config/custom-startup.sh
@@ -2,3 +2,4 @@ echo "This is a custom startup script running inside the client container."
 
 # Install SLEAP
 uv tool install --python 3.13 "sleap[nn]" --index https://download.pytorch.org/whl/cu128 --index https://pypi.org/simple
+export PATH="$HOME/.local/bin:$PATH"
